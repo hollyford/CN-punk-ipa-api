61beaf4515247757b3810274480e400e0e36e88b
Holly Ford <holly_ford@hotmail.com>
2021-11-23 18:16:32 +0000

    added styling to images

--- src/components/BeerInfo.js
+++ src/components/BeerInfo.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import pint from "../images/pint.jpg"
 import FoodButton from "./FoodButton";
 
@@ -5,13 +6,18 @@ const BeerInfo = (props) => {
     return (
         <div>
             <p>Your random beer is: {props.beerName}</p>
-            {props.imgURL ? <img src={props.imgURL} alt={`Picture of ${props.beerName}`} /> : <img src={pint} alt={`Picture of a pint of brewdog`}/>} 
+            {props.imgURL ? <BeerImg src={props.imgURL} alt={`Picture of ${props.beerName}`} /> : <BeerImg src={pint} alt={`Picture of a pint of brewdog`}/>} 
             <p>{props.tagline}</p>
             <p>Description: First brewed in {props.firstBrewed}, {props.description}</p>
             <FoodButton foodPairing={props.foodPairing}/>
         </div>
     )
 }
+
+const BeerImg = styled.img`
+width: auto;
+max-height: 300px;
+`
 
 export default BeerInfo;
 

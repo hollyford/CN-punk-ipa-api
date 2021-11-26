444e78d013208ec2fb645e9182b0a73be4125d26
Holly Ford <holly_ford@hotmail.com>
2021-11-26 18:30:12 +0000

    added show/hide function to food

--- src/components/FoodButton.js
+++ src/components/FoodButton.js
@@ -1,12 +1,14 @@
+import { useState } from "react/cjs/react.development"
 
 const FoodButton = (props) => {
-    
+    const [showFood, setShowFood] = useState(false)
+
+    const onClick = () => setShowFood(true);
+
     return (
         <div>
-            <button>Click here to see what food this pairs well with</button>
-            <div>
-                <p>{props.foodPairing}</p>
-            </div>
+            <button onClick={onClick} >Click here to see what food this pairs well with</button>
+            { showFood ? <p>{props.foodPairing}</p> : "" }
         </div>
     )
 }
